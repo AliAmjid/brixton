81b5862755c14caa59bf4c27eb7218daee5a39e8
Ali <aamjid@artex-is.cz>
2019-05-28 22:10:06 +0000

    file load fix

--- www/index.php
+++ www/index.php
@@ -1,5 +1,9 @@
 <?php
-
+if(isset($_GET['jsHandler'])) {
+	header("Content-Type: application/javascript");
+	echo file_get_contents('ProductSizeHandler.js');
+	exit();
+}
 try {
 	include('app.php');
 } catch (Throwable $e) {
